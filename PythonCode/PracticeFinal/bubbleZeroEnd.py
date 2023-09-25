30fdac4922ac8a49d43f56bb18dcc1b55c418893
manudee <manudee@gmail.com>
2023-09-25 12:19:58 -0400

    added more prbs

--- PythonCode/PracticeFinal/bubbleZeroEnd.py
+++ PythonCode/PracticeFinal/bubbleZeroEnd.py
@@ -1,6 +1,26 @@
 # bubble zeroes in an array to the end
 # similar to bubble sort
 
+# swap zeroes at the end
+def bubbleZerosEnd(arr):
+    # set swapped as False by default
+    swapped  = True
+
+    while swapped:
+        swapped = False
+        for i in range(len(arr) - 1):
+            if(arr[i] == 0 and arr[i+1] != 0 ):
+                # swap
+                arr[i],arr[i+1] = arr[i+1],arr[i]
+                swapped = True
+
+    return arr
+
+val = bubbleZerosEnd([0,1,0,3,4,0,1,3,0])
+print(val)
+
+
+# swap zeroes at the beginning
 def bubbleZerosEnd(arr):
     # set swapped as False by default
     swapped  = True
